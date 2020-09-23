02d7812cfb0102c0a427b87de3c1578a88b5c588
unknown <tjwlsqo44>
2020-09-23 22:05:38 +0900

    test

--- SK/views.py
+++ SK/views.py
@@ -4,17 +4,22 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+import json
+
+
 class Gettest(APIView):
-    def get(self,requset,format=None):
-        st="{" \
-           "\"version\": \"2.0\"," \
-           "\"resultCode\": \"OK\"," \
-           "\"output\": {" \
-           "\"datetime\": \"오늘\"," \
-           "\"TEST\": \"테에스트\"," \
-           "}," \
-           "}"
-        return Response(st)
+    def get(self, requset, format=None):
+        print('abc')
+        dic = {
+            "version": "2.0",
+            "resultCode": "OK",
+            "output": {
+                "datetime": "오늘""",
+                "test": "테에스트",
+                "response": "더럽게 안되네 씨발거"
+            }
+        }
+        jsdic = json.dumps(dic)
 
-
-
+        print(jsdic)
+        return Response(jsdic)
