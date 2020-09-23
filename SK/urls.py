1730053d443d3baa08cd58e9c0c8790d81112e96
unknown <tjwlsqo44>
2020-09-23 23:20:11 +0900

    asdf

--- SK/urls.py
+++ SK/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/test', views.Gettest.as_view()),
+    #path('api/test', views.Gettest.as_view()),
+    path('api/test',views.testapi,name='index'),
 
 ]
--- SK/views.py
+++ SK/views.py
@@ -3,18 +3,19 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 import json
 
 
 class Gettest(APIView):
-    def get(self, requset, format=None):
+    """def get(self, requset, format=None):
         print('abc')
         dic = {
             "version": "2.0",
             "resultCode": "OK",
             "output": {
-                "datetime": "오늘""",
+                "datetime": "오늘",
                 "test": "테에스트",
                 "response": "더럽게 안되네 씨발거"
             }
@@ -23,3 +24,8 @@ class Gettest(APIView):
 
         print(jsdic)
         return Response(jsdic)
+"""
+def testapi(request):
+    dic={"한글":223}
+
+    return JsonResponse(dic)
